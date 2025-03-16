5cc6186b49bda003dfb562aba626b563bac04789
Kjell Schoke <kjell.schoke@outlook.com>
2025-03-16 02:54:41 +0100

    Updated edge runtime for not found

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
+export const runtime = "edge";
+
 export default function Page() {
     return (
         <div className="h-screen w-screen flex items-center justify-center">
