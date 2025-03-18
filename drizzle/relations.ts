6deea65a9d87a8bea457a82884b9f284355b1926
Kjell Schoke <kjell.schoke@outlook.com>
2025-03-18 18:35:53 +0100

    Updated drizzle conf

--- drizzle/relations.ts
+++ drizzle/relations.ts
@@ -1,3 +1,21 @@
 import { relations } from "drizzle-orm/relations";
-import {  } from "./schema";
+import { folders, files } from "./schema";
 
+export const filesRelations = relations(files, ({one}) => ({
+	folder: one(folders, {
+		fields: [files.parentId],
+		references: [folders.id]
+	}),
+}));
+
+export const foldersRelations = relations(folders, ({one, many}) => ({
+	files: many(files),
+	folder: one(folders, {
+		fields: [folders.parentId],
+		references: [folders.id],
+		relationName: "folders_parentId_folders_id"
+	}),
+	folders: many(folders, {
+		relationName: "folders_parentId_folders_id"
+	}),
+}));
