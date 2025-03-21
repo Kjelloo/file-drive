a0b69672fcedf16b5c2b86aede07014c561ac18e
Kjell Schoke <kjell.schoke@outlook.com>
2025-03-21 02:38:33 +0100

    Made s3 path start and end with slash

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -1,8 +1,7 @@
 module "platform" {
   source = "../../module/"
 
-  s3_user = "s3_prod"
-  s3_path = "terraform/prod/s3"
-  s3_user_policy_name = "s3_prod_policy"
-  s3_bucket_name = "schoke_drive_prod"
+  s3_user = "s3-prod"
+  s3_path = "/terraform/prod/s3/"
+  s3_bucket_name = "schoke-drive-prod"
 }
