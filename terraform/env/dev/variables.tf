1b395e7bf065fbfd968217560d8df07b843584ad
Kjell Schoke <kjell.schoke@outlook.com>
2025-03-21 02:13:07 +0100

    Updated variable defaults

--- terraform/env/dev/variables.tf
+++ terraform/env/dev/variables.tf
@@ -1,11 +1,14 @@
 variable "s3_user" {
   type = string
+  default = "s3_dev"
 }
 
 variable "s3_path" {
   type = string
+  default = "terraform/dev/s3"
 }
 
 variable "s3_user_policy_name" {
   type = string
+  default = "s3_dev_policy"
 }
