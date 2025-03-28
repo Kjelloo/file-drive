370f41fe34926fb5f2448f3d5110bbebdb296b5c
Kjell Schoke <kjell.schoke@outlook.com>
2025-03-28 14:00:06 +0100

    removed postgres prepare statement

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres';
 import {files, folders} from "@/db/schema";
 
-const client = postgres(process.env.POSTGRES_URL!, {prepare: false});
+const client = postgres(process.env.POSTGRES_URL!);
 
 export const db = drizzle({ client })
 
